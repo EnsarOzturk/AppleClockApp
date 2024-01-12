990292415222bf250339f0908cff32f86320613b
Ensar Öztürk <ozturkkensar@gmail.com>
2024-01-12 13:24:17 +0300

    AlarmListCell created

---
+++ AppleClockApp/AlarmListCell.swift
@@ -0,0 +1,17 @@
+//
+//  AlarmListCell.swift
+//  AppleClockApp
+//
+//  Created by Ensar on 12.01.2024.
+//
+
+import UIKit
+
+class AlarmListCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
