84b1e566749c2f9c618c9667bcfa23c108e21e6c
Ensar Öztürk <ozturkkensar@gmail.com>
2024-01-12 15:39:58 +0300

    AlarmListHeaderCell created

---
+++ AppleClockApp/AlarmListHeaderCell.swift
@@ -0,0 +1,17 @@
+//
+//  AlarmListHeaderCell.swift
+//  AppleClockApp
+//
+//  Created by Ensar on 12.01.2024.
+//
+
+import UIKit
+
+class AlarmListHeaderCell: UICollectionReusableView {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+    
+}
