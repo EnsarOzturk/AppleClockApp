8dc152b8eec8c75c1e7251424521bb23ec1afbea
Ensar Öztürk <ozturkkensar@gmail.com>
2024-01-25 14:22:31 +0300

    AlarmListCell references were given

--- AppleClockApp/AlarmListViewController.swift
+++ AppleClockApp/AlarmListViewController.swift
@@ -7,20 +7,18 @@
 
 import UIKit
 
-protocol AlarmSaveDelagate: AnyObject {
-    func alarmSaved(hour: String)
-}
-
 class AlarmListViewController: UIViewController {
     @IBOutlet var collectionView: UICollectionView!
+    let alarmlist: [Alarm] = []
+    var alarmKey = "NewAlarmList"
     
-    var viewModel = ClockViewModel()
-
         override func viewDidLoad() {
             super.viewDidLoad()
             
             self.title = "Alarm"
             self.navigationController?.navigationBar.prefersLargeTitles = true
+            collectionView.dataSource = self
+            collectionView.delegate = self
             collectionView.register(UINib(nibName: "AlarmListHeaderCell", bundle: nil), forSupplementaryViewOfKind: UICollectionView.elementKindSectionHeader, withReuseIdentifier: "AlarmListHeaderCell")
 
         }
@@ -36,27 +34,76 @@ class AlarmListViewController: UIViewController {
     }
 }
 
-    extension AlarmListViewController: AlarmSaveDelagate {
-        func alarmSaved(hour: String) {
-            viewModel.addAlarm(hour: hour, isSwitchOn: true)
-            collectionView.reloadData()
-        }
+extension AlarmListViewController: UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
+    
+    func numberOfSections(in collectionView: UICollectionView) -> Int {
+        return 1
     }
-
-    extension AlarmListViewController: UICollectionViewDelegate, UICollectionViewDataSource {
-        func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-            return viewModel.numberOfAlarms
-        }
-
-        func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-            // TODO: Alarm cell configuration using viewModel
-            return UICollectionViewCell()
-        }
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return alarmList.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: identifier, for: indexPath) as! AlarmCell
         
-        // Diğer UICollectionViewDelegate ve UICollectionViewDataSource metodları...
-
-        // TODO: Diğer metodlar ve extension'lar...
+        let alarm = alarmList[indexPath.row]
+        cell.index = indexPath.row
+        cell.delegate = self
+        cell.alarmLabel.text = alarm.hour
+        cell.alarmSwitch.isOn = alarm.isSwitchOn
+        
+        return cell
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        
+        return CGSize(width: UIScreen.main.bounds.width, height: 110)
     }
 
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, referenceSizeForHeaderInSection section: Int) -> CGSize {
+        
+        return CGSize(width: UIScreen.main.bounds.width, height: 120)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, viewForSupplementaryElementOfKind kind: String, at indexPath: IndexPath) -> UICollectionReusableView {
+        
+        var header: HeaderCell!
+        
+        if kind == UICollectionView.elementKindSectionHeader {
+            header =
+            (collectionView.dequeueReusableSupplementaryView(ofKind: kind,
+                                                             withReuseIdentifier: "headerCell", for: indexPath as IndexPath)
+             as? HeaderCell)!
+            
+            return header
+        }else {
+            return UICollectionReusableView()
+        }
+    }
+}
+
+extension AlarmListViewController: AlarmSaveDelagate {
+    func alarmSaved(hour: String) {
+        let alarm = Alarm(hour: hour, isSwitchOn: true)
+        alarmList.append(alarm)
+        
+        alarmCollectionView.reloadData()
+        
+        saveAlarmListToUserDefaults()
+    }
+}
+
+extension AlarmListViewController: AlarmCellDelegate {
+    func alarmSwitchValueChanged(isOn: Bool, index: Int) {
+        
+        alarmList[index].isSwitchOn = isOn
+        saveAlarmListToUserDefaults()
+    }
+}
+
+struct Alarm: Codable {
+    var hour: String
+    var isSwitchOn: Bool
+}
 
 
--- AppleClockApp/Cells/AlarmListCell.swift
+++ AppleClockApp/Cells/AlarmListCell.swift
@@ -7,11 +7,23 @@
 
 import UIKit
 
-class AlarmListCell: UICollectionViewCell {
+protocol AlarmCellDelegate : AnyObject {
+    func alarmSwitchValueChanged(isOn: Bool, index: Int)
+}
 
+class AlarmListCell: UICollectionViewCell {
+    @IBOutlet var alarmClockLabel: UILabel!
+    @IBOutlet var alarmSwitch: UISwitch!
+    
+    weak var delegate : AlarmCellDelegate?
+    var index: Int = 0
+    
     override func awakeFromNib() {
         super.awakeFromNib()
         
     }
 
+    @IBAction func alarmSwitchClick(_ sender: UISwitch) {
+        delegate?.alarmSwitchValueChanged(isOn: sender.isOn, index: index)
+    }
 }
