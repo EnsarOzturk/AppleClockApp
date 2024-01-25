42994626882bf0e97a2a2dc118308f0646dc26af
Ensar Öztürk <ozturkkensar@gmail.com>
2024-01-25 10:57:30 +0300

    Protocol AlarmSaveDelagate created

--- AppleClockApp/AddAlarmViewController.swift
+++ AppleClockApp/AddAlarmViewController.swift
@@ -7,14 +7,31 @@
 
 import UIKit
 
+protocol AddAlarmDelagate: AnyObject {
+    func alarmSaved(hour: String)
+}
+
 class AddAlarmViewController: UIViewController {
     @IBOutlet var saveButton: UIButton!
     @IBOutlet var cancelButton: UIButton!
     @IBOutlet var datePicker: UIDatePicker!
+    @IBOutlet var addAlarmLabel: UILabel!
+    weak var delegate: AddAlarmDelagate?
+
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        changeDatePickerTextColor()
         
+    }
+    private func changeDatePickerTextColor() {
+        datePicker.setValue(UIColor.white, forKeyPath: "textColor")
+    }
+    
+    @IBAction func saveButtonTapped(_ sender: UIButton) {
+        if let hour = addAlarmLabel.text {
+            delegate?.alarmSaved(hour: hour)
+            self.dismiss(animated: true)
+        }
     }
 }
--- AppleClockApp/AlarmListViewController.swift
+++ AppleClockApp/AlarmListViewController.swift
@@ -13,25 +13,49 @@ protocol AlarmSaveDelagate: AnyObject {
 
 class AlarmListViewController: UIViewController {
     @IBOutlet var collectionView: UICollectionView!
-    @IBOutlet var AddAlarmToggleButtonTapped: UIBarButtonItem!
-    @IBOutlet var AlarmEditButtonTapped: UIBarButtonItem!
     
     var viewModel = ClockViewModel()
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        
-        self.title = "Alarm"
-        self.navigationController?.navigationBar.prefersLargeTitles = true
-        collectionView.register(UINib(nibName: "AlarmListHeaderCell", bundle: nil), forSupplementaryViewOfKind: UICollectionView.elementKindSectionHeader, withReuseIdentifier: "AlarmListHeaderCell")
+        override func viewDidLoad() {
+            super.viewDidLoad()
+            
+            self.title = "Alarm"
+            self.navigationController?.navigationBar.prefersLargeTitles = true
+            collectionView.register(UINib(nibName: "AlarmListHeaderCell", bundle: nil), forSupplementaryViewOfKind: UICollectionView.elementKindSectionHeader, withReuseIdentifier: "AlarmListHeaderCell")
+
+        }
+    
+    @IBAction func alarmEditButtonTapped(_ sender: Any) {
+    }
+    
+    @IBAction func addAlarmToggleButtonTapped(_ sender: Any) {
+        let storyboard = UIStoryboard(name: "Main", bundle: nil)
+               let addVC = storyboard.instantiateViewController(identifier: "AddAlarmViewController") as! AddAlarmViewController
+               addVC.modalPresentationStyle = .fullScreen
+               present(addVC, animated: true, completion: nil)
     }
 }
 
-extension AlarmListViewController: AlarmSaveDelagate {
+    extension AlarmListViewController: AlarmSaveDelagate {
         func alarmSaved(hour: String) {
             viewModel.addAlarm(hour: hour, isSwitchOn: true)
             collectionView.reloadData()
         }
+    }
+
+    extension AlarmListViewController: UICollectionViewDelegate, UICollectionViewDataSource {
+        func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+            return viewModel.numberOfAlarms
+        }
+
+        func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+            // TODO: Alarm cell configuration using viewModel
+            return UICollectionViewCell()
+        }
+        
+        // Diğer UICollectionViewDelegate ve UICollectionViewDataSource metodları...
+
+        // TODO: Diğer metodlar ve extension'lar...
     }
 
 
