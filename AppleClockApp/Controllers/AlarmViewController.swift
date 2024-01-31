aca13358516caabfb52e9e1b606c046c94b873ad
Ensar Öztürk <ozturkkensar@gmail.com>
2024-01-31 17:00:30 +0300

    AlarmViewController storyboard created

--- AppleClockApp/Controllers/AlarmViewController.swift
+++ AppleClockApp/Controllers/AlarmViewController.swift
@@ -7,23 +7,18 @@
 
 import UIKit
 
-class AlarmViewController: UIViewController {
 
+class AlarmViewController: UIViewController {
+    @IBOutlet var alarmLabel: UILabel!
+    @IBOutlet var closeButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @IBAction func closeButtonTapped(_ sender: UIButton) {
     }
-    */
-
+    
 }
