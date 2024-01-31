880463f044566edadd6a850bd6de1313533a235f
Ensar Öztürk <ozturkkensar@gmail.com>
2024-01-31 22:28:58 +0300

    StopWatchListCell folder created

---
+++ AppleClockApp/Module/Cells/StopWatchListCell/StopWatchListCell.swift
@@ -0,0 +1,16 @@
+//
+//  StopWatchListCell.swift
+//  AppleClockApp
+//
+//  Created by Ensar on 31.01.2024.
+//
+
+import UIKit
+
+class StopWatchListCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        
+    }
+}
